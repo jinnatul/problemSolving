9e2a313a5332cf3995d43bdf0f08886537fbad92
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-04-12 23:29:30 +0600

    Add files via upload

---
+++ Codeforces gym 102163D. Football Cup NCD 2019.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n", a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define mx 1000000
+#define mod 1000000007
+#define PI acos(-1.0)
+
+#define size1 100010
+
+int drx[8] = {-2,-2,-1,-1,1,1,2,2};
+int dcy[8] = {-1,1,-2,2,-2,2,-1,1};
+
+int dirx[4] = { -1, 0, 1, 0 };
+int diry[4] = { 0, -1, 0, 1 };
+
+ll gcd(ll a,ll b){ if(b == 0) return a; return gcd(b, a % b); }
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+
+int main()
+{
+    ll tc;
+
+    sc1(tc);
+
+    while(tc--){
+        ll x, y;
+        sc2(x, y);
+
+        if(x == y) cout << "Iskandar" << endl;
+        else if(x < y) cout << "Hamada" << endl;
+        else cout << "Bashar" << endl;
+    }
+}
