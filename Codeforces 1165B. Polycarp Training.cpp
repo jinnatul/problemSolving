5088fd6b8e1ffb8c19e27db9030ae1d4e20f2ce7
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-05-27 00:37:41 +0600

    Create Codeforces 1165B. Polycarp Training.cpp

---
+++ Codeforces 1165B. Polycarp Training.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native"
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n", a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define size1 105
+
+int main() {
+
+    //seive();
+    //preCal();
+
+    ll tc, num, t = 1, choose;
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+    //freopen("/opt/Coding/clion code/output.txt", "w", stdout);
+
+    sc1(num);
+
+    ll arr[num];
+    for(ll i = 0; i < num; i++){
+        sc1(arr[i]);
+    }
+
+    sort(arr, arr + num);
+
+    ll ans = 1;
+    for(ll i = 0; i < num; i++){
+        if(arr[i] >= ans) ans++;
+    }
+
+    pf1(ans - 1);
+
+    return 0;
+}
