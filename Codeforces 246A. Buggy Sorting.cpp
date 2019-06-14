f5f8d3a147b978bb5617e685aa68f56456b11b1d
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-06-14 19:47:27 +0600

    Create Codeforces 246A. Buggy Sorting.cpp

---
+++ Codeforces 246A. Buggy Sorting.cpp
@@ -0,0 +1,41 @@
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native"
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n", a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define size1 300005
+
+
+int main() {
+
+    //seive();
+    //preCal();
+
+    ll tc, num, t = 1, choose;
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+    //freopen("/opt/Coding/clion code/output.txt", "w", stdout);
+
+
+    ll sum1 = 0, sum2 = 0;
+
+    sc1(num);
+
+    if(num < 3) return cout << "-1" << endl,0;
+
+    for(ll i = num; i >= 1; i--){
+        cout << i << " ";
+    }
+
+    return 0;
+}
