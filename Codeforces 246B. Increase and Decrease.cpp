c2a1f45bd728eb83e3e325a2b2f4532c88c7e595
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-06-14 19:49:45 +0600

    Create Codeforces 246B. Increase and Decrease.cpp

---
+++ Codeforces 246B. Increase and Decrease.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native"
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n", a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define size1 300005
+
+
+int main() {
+
+    //seive();
+    //preCal();
+
+    ll tc, num, t = 1, choose;
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+    //freopen("/opt/Coding/clion code/output.txt", "w", stdout);
+
+
+    ll sum = 0;
+
+    sc1(num);
+
+    for(ll i = 0; i < num; i++){
+        ll x;
+        sc1(x);
+        sum += x;
+    }
+
+    if(sum % num == 0) cout << num << endl;
+    else cout << num -1 << endl;
+
+    return 0;
+}
