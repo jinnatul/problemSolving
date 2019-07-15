7cbe40771326350bb83dcae87f8e94ebb880fff1
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-07-15 17:30:18 +0600

    Create Codeforces 1194A. Remove a Progression.cpp

---
+++ Codeforces 1194A. Remove a Progression.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+/// Typedef
+typedef long long ll;
+ 
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+ 
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+ 
+#define mx 10000007
+#define mod 100000007
+#define PI acos(-1.0)
+ 
+#define size1 1000
+#define pb push_back
+ 
+ 
+int main()
+{
+    ll k, num, m, tc, t = 1;
+ 
+    sc1(tc);
+ 
+    for(ll i = 0 ; i < tc; i++){
+       sc2(m, num);
+       pf1(num * 2);
+    }
+}
