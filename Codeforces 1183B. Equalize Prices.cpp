192ce4ad1df1f6e4b0ade037b0b4138f3550995b
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-07-18 13:19:47 +0600

    Create Codeforces 1183B. Equalize Prices.cpp

---
+++ Codeforces 1183B. Equalize Prices.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+ 
+/// Typedef
+typedef long long ll;
+ 
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+ 
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+ 
+#define mx 100007
+#define mod 100000007
+#define PI acos(-1.0)
+ 
+#define size1 12
+#define pb push_back
+ 
+ 
+int main()
+{
+ 
+    ll k, num, prime, m, tc, t = 1;
+ 
+    sc1(tc);
+ 
+    while (tc--){
+ 
+        sc2(num, m);
+        ll arr[num];
+ 
+        ll ans = INT_MAX;
+        for(ll i = 0; i < num; i++){
+            sc1(arr[i]);
+            ans = min(ans, arr[i]);
+        }
+ 
+        ans += m;
+ 
+        bool ck = true;
+        for(ll i = 0; i < num; i++){
+            if(abs(ans - arr[i]) > m){
+                ck = false;
+                break;
+            }
+        }
+ 
+        if(ck == true) cout << ans << endl;
+        else cout << "-1" << endl;
+    }
+ 
+}
