0e2b8618667ad855b4c73f93657024f75744b96f
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-07-19 13:12:51 +0600

    Update Codeforces 1180C. Valeriy and Deque.cpp

--- Codeforces 1180C. Valeriy and Deque.cpp
+++ Codeforces 1180C. Valeriy and Deque.cpp
@@ -95,3 +95,80 @@ int main()
         }
     }
 }
+
+
+OR
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define mx 100007
+#define mod 100000007
+#define PI acos(-1.0)
+
+#define size1 12
+#define pb push_back
+
+pair <ll, ll> ans[mx];
+
+int main()
+{
+
+    ll k, num, prime, m, tc, t = 4;
+
+    sc2(num, tc);
+
+    ll arr[num];
+
+    deque <ll> dq;
+
+    ll mxx = 0;
+    for(ll i = 0; i < num; i++){
+       sc1(m);
+       dq.push_back(m);
+       mxx = max(mxx, m);
+    }
+
+    for(ll i = 1; i < num; i++){
+        ll x = dq.front(); dq.pop_front();
+        ll y = dq.front(); dq.pop_front();
+
+        ans[i] = {x, y};
+
+        dq.push_front(max(x, y));
+        dq.push_back(min(x, y));
+    }
+
+    ll firMax = dq[0], secMax = 0;
+    dq.pop_front();
+
+    while (tc--) {
+        ll pos;
+        sc1(pos);
+
+        if(pos < num){
+            cout << ans[pos].first << " " << ans[pos].second << endl;
+        }
+        else{
+
+            secMax = (pos - num) % dq.size();
+
+            cout << firMax << " " << dq[secMax] << endl;
+        }
+    }
+}
