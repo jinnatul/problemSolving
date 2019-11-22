2e90d3e47e0d6e6b7098db4c2ad937f4df138100
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-11-22 19:15:17 +0600

    Create codeforces 264A. Escape from Stones.cpp

---
+++ codeforces 264A. Escape from Stones.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define vpnt(ans) for(ll i = 0; i < ans.size(); i++) cout << ans[i] << (i + 1 < ans.size() ? ' ' : '\n');
+#define apnt(arr, num) for(ll i = 0; i < num; i++) cout << arr[i] << (i + 1 < num ? ' ' : '\n');
+
+#define mx 100007
+#define mod 1000000007
+#define PI acos(-1.0)
+#define size1 10001
+
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define case cout << "Case " << t++ << ": ";
+
+typedef vector <ll> vll;
+typedef set <ll> sll;
+typedef map <ll, ll> mll;
+typedef pair <ll, ll> pll;
+typedef vector <pair <ll , ll> > vpll;
+
+
+int main() {
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+
+    ll m, tc, num, t = 1;
+
+    string str;
+    cin >> str;
+
+    vll left, right;
+    for (ll i = 0; i < str.size(); ++i) {
+        if(str[i] == 'l') left.pb(i + 1);
+        else right.pb(i + 1);
+    }
+
+    for (ll i = 0; i < right.size(); ++i) {
+        cout << right[i] << "\n";
+    }
+
+    reverse(left.begin(), left.end());
+    for (ll i = 0; i < left.size(); ++i) {
+        cout << left[i] << "\n";
+    }
+}
