92cd1a989d8f6f06db9fd428b7482d976d0d8329
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-12-15 22:27:25 +0600

    Create codeforces 1271A. Suits.cpp

---
+++ codeforces 1271A. Suits.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define vpnt(ans) for(ll i = 0; i < ans.size(); i++) cout << ans[i] << (i + 1 < ans.size() ? ' ' : '\n');
+#define apnt(arr, num) for(ll i = 0; i < num; i++) cout << arr[i] << (i + 1 < num ? ' ' : '\n');
+
+#define mx 200005
+#define mod 1000000007
+#define PI acos(-1.0)
+#define eps 1e-7
+
+#define size1 200050
+
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define case cout << "Case " << t++ << ": ";
+
+typedef vector <ll> vll;
+typedef set <ll> sll;
+typedef queue <ll> qll;
+typedef map <ll, ll> mll;
+typedef pair <ll, ll> pll;
+typedef vector <pair <ll , ll> > vpll;
+
+int main() {
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+
+    ll n, m, tc, num, t = 1;
+
+    ll a, b, c, d, e, f;
+    cin >> a >> b >> c;
+    cin >> d >> e >> f;
+
+    ll m1 = min(a, d);
+    ll ans1 = (m1*e) + min(b, min(c, d - m1)) * f;
+
+    ll m2 = min(b, min(c, d));
+    ll ans2 = (m2*f) + min(a, d-m2) * e;
+
+    cout << max(ans1, ans2) << endl;
+}
