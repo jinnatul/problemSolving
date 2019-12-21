bda1bf7e27697c2ec8fd041b2deffe1629ba81a5
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2019-12-21 21:50:51 +0600

    Create codeforces 1269B. Modulo Equality.cpp

---
+++ codeforces 1269B. Modulo Equality.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define vpnt(ans) for(ll i = 0; i < ans.size(); i++) cout << ans[i] << (i + 1 < ans.size() ? ' ' : '\n');
+#define apnt(arr, num) for(ll i = 0; i < num; i++) cout << arr[i] << (i + 1 < num ? ' ' : '\n');
+
+#define mx 200005
+#define mod 1000000007
+#define PI acos(-1.0)
+#define eps 1e-7
+
+#define size1 200005
+
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define case cout << "Case " << t++ << ": ";
+
+typedef vector <ll> vll;
+typedef set <ll> sll;
+typedef queue <ll> qll;
+typedef map <ll, ll> mll;
+typedef pair <ll, ll> pll;
+typedef vector <pair <ll , ll> > vpll;
+
+
+int main() {
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+
+    ll n, m, tc, num, t = 1;
+
+    sc2(num, m);
+
+    vll v1, v2;
+    for (ll i = 0; i < num; ++i) {
+        sc1(n);
+        v1.pb(n);
+    }
+
+    for (ll i = 0; i < num; ++i) {
+        sc1(n);
+        v2.pb(n);
+    }
+
+    sort(v2.begin(), v2.end());
+
+    ll ans = INT_MAX;
+    for (ll i = 0; i < num; ++i) {
+        vll temp;
+        ll avgmod = (m + v2[i] - v1[0]) % m;
+        for (ll j = 0; j < num; ++j) {
+            temp.pb((v1[j] + avgmod) % m);
+        }
+        sort(temp.begin(),temp.end());
+
+        if(v2 == temp){
+            ans = min(ans, avgmod);
+        }
+    }
+
+    pf1(ans);
+}
