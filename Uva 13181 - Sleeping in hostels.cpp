cff0cb2ee346a1a82daa96394ea739c735186359
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-01-13 13:56:58 +0600

    Create Uva 13181 - Sleeping in hostels.cpp

---
+++ Uva 13181 - Sleeping in hostels.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+/// Typedef
+typedef long long ll;
+
+#define sc1(a) scanf("%lld",&a)
+#define sc2(a,b) scanf("%lld %lld",&a,&b)
+
+#define pf1(a) printf("%lld\n",a)
+#define pf2(a,b) printf("%lld %lld\n",a,b)
+
+#define vpnt(ans) for(ll i = 0; i < ans.size(); i++) cout << ans[i] << (i + 1 < ans.size() ? ' ' : '\n');
+#define apnt(arr, num) for(ll i = 0; i < num; i++) cout << arr[i] << (i + 1 < num ? ' ' : '\n');
+
+#define mx 200005
+#define mod 1000000007
+#define PI acos(-1.0)
+#define eps 1e-7
+
+#define size1 1000100
+
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define case cout << "Case " << t++ << ": ";
+
+typedef vector <ll> vll;
+typedef set <ll> sll;
+typedef queue <ll> qll;
+typedef map <ll, ll> mll;
+typedef pair <ll, ll> pll;
+typedef vector <pair <ll , ll> > vpll;
+
+inline bool cmp(pll a,pll b){ if(a.ff == b.ff)return a.ss < b.ss; return a.ff > b.ff; }
+
+
+int main() {
+
+    //freopen("/opt/Coding/clion code/input.txt", "r", stdin);
+
+    ll n, m, num, tc, t = 1;
+
+    string str;
+    while (cin >> str) {
+        ll dis = 0, temp = 0, left = 0, right = 0, pos = 0;
+
+        for (ll i = 0; i < str.length(); ++i) {
+            if(str[i] == 'X') {
+                pos = i;
+                break;
+            }
+            else left++;
+        }
+
+        for (ll i = str.length() - 1; i >= 0; i--) {
+            if(str[i] == 'X') break;
+            else right++;
+        }
+
+        ll ans = max(left, right) - 1;
+
+        for (ll i = pos; i < str.length(); ++i) {
+            if(str[i] == 'X') {
+                dis = max(dis, temp);
+                temp = 0;
+            }
+            else temp++;
+        }
+        dis = max(dis, temp);
+
+        temp = (dis + 1)/ 2;
+        temp--;
+
+        if(temp > ans) ans = temp;
+
+        pf1(ans);
+    }
+}
