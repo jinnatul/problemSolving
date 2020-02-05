64ac608500e8a06946d6c67405ad68b0f66369ce
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-05 15:11:31 +0600

    Create bouble_sort.cpp

---
+++ bouble_sort.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main() {
+    int n,i,ln,j,a,b,t,z,ck,temp;
+     
+     printf("size : ");
+    scanf("%d",&n);
+    
+    int arr[n];
+   
+    printf("Element : ");
+    for(i=0;i<n;i++){
+
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0;i<n-1;i++){
+        
+        for(j=0;j<(n-1-i);j++){
+           
+            if(arr[j]>arr[j+1]){
+              
+                temp=arr[j];
+               
+                arr[j]=arr[j+1];
+                
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++){
+       
+        printf("%d ",arr[i]);
+    }
+
+  
+}
