c37440781791893e09e27381448c9bf972de75e4
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-07 03:15:20 +0600

    Create crismas uri 2139.cpp

---
+++ crismas uri 2139.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c,t;
+    while(scanf("%d%d",&a,&b) !=EOF){
+        if(a==12 && b==25){
+            printf("E natal!\n");
+        }else if(a==12 && b<25){
+            printf("E vespera de natal!\n");
+        }else if(a==12 && b>25){
+        printf("Ja passou!\n");
+        }else if(a<12 && (b>25 || b<25)){
+       int array[13]={0,31,29,31,30,31,30,31,31,30,31,30,25};
+        int sum =0;
+        int i,m,d;
+        m=a;d=b;
+        for(i=m+1;i<=12;i++){
+        sum +=array[i];
+        }
+        int pp =array[m]-d;
+        sum +=pp;
+        printf("Faltam %d dias para o natal!\n",sum);
+
+        }else{ }
+
+    }
+
+    return 0;
+}
