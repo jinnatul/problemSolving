b81d3bf4e0a8d1624d4669acedbb2afc3b354bc9
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-06 20:48:08 +0600

    Create array_secondary_diagonal_sum.cpp

---
+++ array_secondary_diagonal_sum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+ 
+int main()
+{
+	int a,b,n,i,j,key,mid,start,sum,ck,end;
+    int arr[100][100];
+	cin>>n;
+	arr[n];
+
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			cin>>arr[i][j];
+		}		
+	}
+
+    
+    sum=0;
+   	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(i+j==n-1){
+				sum+=arr[i][j];
+			}
+		}			
+	}
+	cout<<sum<<endl;	
+   
+}
