2d338b601e52dcec762658d1d27caf38ac75c2ed
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-07 03:11:00 +0600

    Create Squared and Cubic uri 1143.cpp

---
+++ Squared and Cubic uri 1143.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,i,e;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        printf("%d %d %d\n",i,c,d);
+        c=i*i;
+        d=c*i;
+
+    }
+    return 0;
+}
