35355952bb36e1cb8ac100eded7df46fd0c65796
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-07 03:01:29 +0600

    Create Og uri 1387.cpp

---
+++ Og uri 1387.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    while(1){
+            //if(1<=a && b<=5){
+            scanf("%d%d",&a,&b);
+        if(a==0 && b==0)
+        break;
+    c=a+b;
+    printf("%d\n",c);
+            }
+
+    return 0;
+}
