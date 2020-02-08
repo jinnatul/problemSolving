a8cc7e11e25f98b43685cfe4b553ecea1b77785a
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-08 19:35:36 +0600

    Create uva 10940 Throwing cards away II.cpp

---
+++ uva 10940 Throwing cards away II.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    long int n,i,mod,result;
+    while(scanf("%ld",&n)==1 && n!=0)
+    {
+        i=1;
+        while(i<=n)
+        {
+            i=i*2;
+        }
+        
+        mod=i%n;
+        result=n-mod;
+        printf("%ld\n",result);
+    }
+
+}
