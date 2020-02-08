c2056e0aed63eaabaec156fbe43987cb4f697a57
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-08 19:32:31 +0600

    Create uva 10055 Hashmat the Brave Warrior.cpp

---
+++ uva 10055 Hashmat the Brave Warrior.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+//int gcd(int a, int b) { if (b == 0)  return a;   else return gcd(b, a%b); }
+//int lcm(int a, int b) { return (a*b)/gcd(a,b); }
+
+int main()
+{
+    long long int n,i,a,b,t,j,aa,ck=0;
+     
+     while(scanf("%lld %lld", &a, &b) != EOF){
+ 
+     if(a<b){
+        ck = b-a;
+        printf("%lld\n", ck);
+     }
+     else{
+        ck = (abs(a-b));
+         printf("%lld\n", ck);
+     }
+
+    }
+}
