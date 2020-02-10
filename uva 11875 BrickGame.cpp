873e90f701df92acb2034e8bdfe79c828fe1fd01
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-10 21:10:04 +0600

    Create uva 11875 BrickGame.cpp

---
+++ uva 11875 BrickGame.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,i,n,j,start,end,mid;
+    cin>>n;
+
+    start =0;
+    end = t-1;
+
+    for(i=1;i<=n;i++){
+        cin>>t;
+        int arr[t];
+
+        for(j=0; j<t; j++){
+            cin>>arr[j];
+        }
+        sort(arr,arr+t);
+
+        end = t/2;
+
+        printf("Case %d: %d\n",i , arr[end]);
+
+    }
+
+}
