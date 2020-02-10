13738641a69a6a3ad415fbb60d7306b1665ee93d
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-10 21:06:55 +0600

    Create uva 1124 Celebrity jeopardy.cpp

---
+++ uva 1124 Celebrity jeopardy.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<string.h>
+#include<stdio.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    string temp;
+    while (getline(cin, temp))
+    {
+        cout << temp << '\n';
+    }
+
+}
