964971c8375f0bb36ca7517afb775264a4aaf71d
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-20 21:46:15 +0600

    Create codeforces gym 101291F - Equality.cpp

---
+++ codeforces gym 101291F - Equality.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int q,w,e;
+    scanf("%d + %d = %d",&q,&w,&e);
+    if(q+w==e)printf("YES\n");
+    else printf("NO\n");
+    return 0;
+}
