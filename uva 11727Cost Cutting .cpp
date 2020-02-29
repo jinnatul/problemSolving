0fe4bad33513a15ef9c01e6c476878c21599ebfd
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-02-29 21:51:32 +0600

    Create uva 11727Cost Cutting .cpp

---
+++ uva 11727Cost Cutting .cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+ 
+using namespace std;
+
+int main()
+{
+    int n,t,i,a,b,c,sum,max,j;
+
+    cin>>n;
+
+    for(i=1; i<=n; i++){
+        cin>>a >> b >>c;
+
+        if(b>a && a>c || b<a && a<c){
+            
+            printf("Case %d: %d\n",i, a);
+        }
+        else if(a>b && b>c || a<b && b<c){
+
+             printf("Case %d: %d\n",i, b);
+        }
+        else if(a>c && c>b || a<c && c<b){
+
+             printf("Case %d: %d\n",i, c);
+        }
+    }
+
+}
